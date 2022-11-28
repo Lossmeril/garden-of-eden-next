1716e7cdaec4cdf0582f43715df3db783caae234
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2022-11-28 16:56:17 +0000

    feat: Google Analytics integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../layouts/layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SQBGDSK0QZ"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SQBGDSK0QZ');
+    `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
