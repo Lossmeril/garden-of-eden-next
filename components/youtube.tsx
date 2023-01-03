0936bbe7b2499f06eb39ed6c049aee29fa1c6c64
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-01-03 17:51:10 +0100

    feat: new trailer link + thumbnails

--- components/youtube.tsx
+++ components/youtube.tsx
@@ -1,7 +1,13 @@
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
 const YouTubeTrailer = () => {
-  return <LiteYouTubeEmbed id="4LNwuwqlK-4" title="Zahrada Boží Trailer" />;
+  return (
+    <LiteYouTubeEmbed
+      id="yhdX3suLNLI"
+      title="Zahrada Boží (2023) | Oficiální český trailer"
+      thumbnail="/img/thumbnail-cz.webp"
+    />
+  );
 };
 
 export default YouTubeTrailer;
