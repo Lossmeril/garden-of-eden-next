28c9ecd77632df173edfbd06055c58542144db1c
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-01-25 18:34:04 +0100

    fix: hithit link

--- datasets/links.tsx
+++ datasets/links.tsx
@@ -1,6 +1,6 @@
 const links = {
   hitHit:
-    "https://www.hithit.com/cs/project/11125/zahrada-bozi-dokumentarni-film",
+    "https://www.hithit.com/cs/project/11125/pomozte-nam-vykopnout-dokumentarni-film-zahrada-bozi",
 };
 
 export default links;
